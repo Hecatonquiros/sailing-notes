d08d03823aa1b1229c5605529d0c9ac4caabe21d
Héctor Gutiérrez <hecatonquiros@protonmail.com>
2025-09-30 01:12:44 +0200

    Fix error of METADATA

--- src/consts.ts
+++ src/consts.ts
@@ -48,7 +48,6 @@ export const SITE: Site = {
   NAME: "Sailing Notes",
   EMAIL: "hectorgutialo@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 3,
-  NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
